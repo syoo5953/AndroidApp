670fc66eb001da49174707563647927992050c66
Sungki Yoo <gurush@naver.com>
2021-11-14 19:25:02 +0900

    updated new app

---
+++ FaceEmotionDetection/app/src/main/java/com/lot/fec/FacialActivity.java
@@ -0,0 +1,127 @@
+package com.lot.fec;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.lot.fec.Helper.GraphicOverlay;
+import com.lot.fec.Helper.RectOverlay;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+
+public class FacialActivity extends AppCompatActivity {
+
+    private Button faceDetectButton;
+    private GraphicOverlay graphicOverlay;
+    private CameraView cameraView;
+    AlertDialog alertDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facial);
+
+        faceDetectButton = findViewById(R.id.detect_face_btn);
+        graphicOverlay = findViewById(R.id.graphic_overlay);
+        cameraView = findViewById(R.id.camera_view);
+        alertDialog = new SpotsDialog.Builder().setContext(this).setMessage("Please Wait, Processing...").setCancelable(false).build();
+        faceDetectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.start();
+                cameraView.captureImage();
+                graphicOverlay.clear();
+            }
+        });
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                alertDialog.show();
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap, cameraView.getWidth(), cameraView.getHeight(),
+                        false);
+                cameraView.stop();
+
+                processFaceDetection(bitmap);
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+    }
+
+    private void processFaceDetection(Bitmap bitmap) {
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionFaceDetectorOptions firebaseVisionFaceDetectorOptions = new FirebaseVisionFaceDetectorOptions.Builder().build();
+        FirebaseVisionFaceDetector firebaseVisionBarcodeDetector = FirebaseVision.getInstance().getVisionFaceDetector(firebaseVisionFaceDetectorOptions);
+
+        firebaseVisionBarcodeDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+                getFaceResult(firebaseVisionFaces);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e) {
+                Toast.makeText(FacialActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getFaceResult(List<FirebaseVisionFace> firebaseVisionFaces) {
+        int counter = 0;
+        for(FirebaseVisionFace face : firebaseVisionFaces) {
+            Rect rect = face.getBoundingBox();
+            RectOverlay rectOverlay = new RectOverlay(graphicOverlay,rect);
+            graphicOverlay.add(rectOverlay);
+            counter++;
+        }
+        alertDialog.dismiss();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.stop();
+    }
+}
---
+++ FaceEmotionDetection/app/src/main/java/com/lot/fec/Helper/RectOverlay.java
@@ -0,0 +1,40 @@
+package com.lot.fec.Helper;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+public class RectOverlay extends GraphicOverlay.Graphic{
+    private int mRectColor = Color.GREEN;
+    private float StrokeWidth = 4.0f;
+    private Paint mRectPaint;
+    private GraphicOverlay graphicOverlay;
+    private Rect rect;
+
+    public RectOverlay(GraphicOverlay graphicOverlay, Rect rect) {
+        super(graphicOverlay);
+        mRectPaint = new Paint();
+        mRectPaint.setColor(mRectColor);
+        mRectPaint.setStyle(Paint.Style.STROKE);
+        mRectPaint.setStrokeWidth(StrokeWidth);
+
+        this.graphicOverlay = graphicOverlay;
+        this.rect = rect;
+
+        postInvalidate();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        RectF  rectF = new RectF(rect);
+        rectF.left = translateX(rectF.left);
+        rectF.right = translateX(rectF.right);
+        rectF.top = translateX(rectF.top);
+        rectF.bottom = translateX(rectF.bottom);
+
+        canvas.drawRect(rectF, mRectPaint);
+
+    }
+}
